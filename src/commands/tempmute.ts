b2bf05d75e4fe8d8b402ed14095613f80dabc620
ahmednet <ahmednet03@gmail.com>
2019-04-08 12:22:09 +0200

    tempmute

---
+++ src/commands/tempmute.ts
@@ -0,0 +1,43 @@
+import * as Discord from 'discord.js'
+import * as ms from 'ms'
+
+export function run (message:Discord.Message, bot, cmd):any {
+	let user = message.guild.member(message.mentions.users.first());
+	let time = message.content.split(" ")[1];
+	let muterole:any = message.guild.roles.find(`name`, "muted");
+	if(!user) return message.reply("Couldn't find user.");
+	if(!message.member.hasPermission("MANAGE_ROLES")) return message.reply("you Can't mute them!");
+	// start of create role
+	if(!muterole){
+		try{
+		 muterole = message.guild.createRole({
+		  	"name": "muted",
+	        "color": "#000000",
+	        "permissions":[]
+		  });
+		} catch(e){
+      		console.log(e.message);
+      		message.reply(e.message);
+    	}
+	}
+	 message.guild.channels.forEach((channel, id) => {
+         channel.overwritePermissions(muterole, {
+          SEND_MESSAGES: false,
+          ADD_REACTIONS: false
+        });
+      });
+	 //end of create role
+	 if(!time) return message.reply("You didn't specify a time!");
+	 user.addRole(muterole.id);
+	 message.channel.send(`<@${user.id}> has been muted for ${ms(time)} milliseconds`);
+	setTimeout(() => {
+	    user.removeRole(muterole.id);
+	    message.channel.send(`<@${user.id}> has been unmuted!`);
+	}, ms(time));
+
+
+  //end of module
+
+}
+
+export const help = {name:"tempmute"}
